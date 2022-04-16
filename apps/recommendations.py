06800966905f3eb0a86b8c9fb84d52ee6aeed948
itayalroy <itayalroy@gmail.com>
2022-04-16 15:05:51 +0300

    basic collaborative filtering done

--- apps/recommendations.py
+++ apps/recommendations.py
@@ -6,13 +6,15 @@ num_recommendations = 5
 thres = 0  # we will not consider users with less than this number of courses that current user has taken
 
 
-def get_closest_users(curr_user: Users):
+def get_closest_users(curr_user: Users, k=5):
+    """ Returns a list of tuples of the form (user_id, avg_rating_diff), sorted by avg_rating_diff, for user_ids that
+     have at least thres number of courses that current user has taken. """
     candidates = {}
     UC1 = aliased(UsersCourses)
     similar_courses = db.session.query(UsersCourses, UC1).join(UC1, UsersCourses.course_id == UC1.course_id).filter(
         UsersCourses.user_id == curr_user.id, UsersCourses.status == 2, UC1.status == 2).all()
-    print(similar_courses)
-    print(len(similar_courses))
+    # print(similar_courses)
+    # print(len(similar_courses))
     for pair in similar_courses:
         if pair[1].user_id not in candidates:
             candidates[pair[1].user_id] = (0, 0)  # first is sum of rating diff, second is number of courses
@@ -24,23 +26,35 @@ def get_closest_users(curr_user: Users):
     closest_users = [(user_id, candidates[user_id][0] / candidates[user_id][1]) for user_id in candidates if
                      candidates[user_id][1] >= thres]
     closest_users.sort(key=lambda x: x[1])
-    print(closest_users)
-    return closest_users
+    # print(closest_users)
+    return closest_users[:k]
 
 
-# def get_highest_rated_untaken_courses(curr_user: Users, closest_users: list):
-#     taken_courses = [course.course_id for course in curr_user.courses]
-#     untaken_courses = Courses.query.filter(Courses.id.notin(taken_courses)).all()
-#     for course in untaken_courses:
-#
-#     return untaken_courses[:num_recommendations]
+def get_highest_rated_untaken_courses(curr_user: Users, closest_users: list):
+    """" Returns a list of """
+    taken_courses = [course.course_id for course in curr_user.courses]
+    untaken_courses = Courses.query.filter(Courses.id.notin_(taken_courses)).all()
+    closest_users_opinions = UsersCourses.query.filter(UsersCourses.user_id.in_(closest_users),
+                                                       UsersCourses.status == 2).all()
+    untaken_courses = {course.id: (course, 0, 0) for course in
+                       untaken_courses}  # first is course, second is sum of ratings, third is number of ratings
+    for opinion in closest_users_opinions:
+        if opinion.course_id in untaken_courses:
+            untaken_courses[opinion.course_id] = (
+                untaken_courses[opinion.course_id][0], untaken_courses[opinion.course_id][1] + opinion.rating,
+                untaken_courses[opinion.course_id][2] + 1)
+    untaken_courses = sorted(untaken_courses.values(), key=lambda x: x[1] / x[2], reverse=True)
+    return untaken_courses[:num_recommendations]
 
 
 def collaborative_filtering(curr_user: Users):
-    closest_users = get_closest_users(curr_user)
-
-    return closest_users
+    # TODO: calculate k
+    k = 5
+    closest_users = get_closest_users(curr_user, k)
+    recommendations = get_highest_rated_untaken_courses(curr_user, [pair[0] for pair in closest_users])
+    return recommendations[:num_recommendations]
 
 
 def recommend_courses(curr_user: Users):
+    # TODO: add another recommendation method
     return collaborative_filtering(curr_user)
