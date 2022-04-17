60e32205b53df49b79526b069871d55f80e3889e
itayalroy <itayalroy@gmail.com>
2022-04-18 02:56:53 +0300

    comments are added to the db

--- apps/course_page/routes.py
+++ apps/course_page/routes.py
@@ -3,10 +3,13 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, render_template_string
+from flask_login import current_user, login_required
 from apps.authentication import blueprint
 from apps.authentication.models import *
 
+
+@login_required
 @blueprint.route('course=<course_id>')
 def course(course_id):
     course = Courses.query.filter_by(id=course_id).first()
@@ -17,3 +20,23 @@ def course(course_id):
         question = Questions.query.filter_by(id=question_id).first()
         features.append(('id' + str(question.id), question.feature, answer.sum_ratings / answer.num_ratings))
     return render_template('home/course.html', course=course, featurs=features)
+
+
+@login_required
+@blueprint.route('course=<course_id>', methods=['POST'])
+def submit_comment(course_id):
+    text = request.form['comment']
+    user = current_user
+    comment = Comments(comment=text, user_id=user.id, course_id=course_id)
+    db.session.add(comment)
+    db.session.commit()
+    return course(course_id)
+
+
+    # id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    # course_id = db.Column(db.Integer, db.ForeignKey('Courses.id'))
+    # comment = db.Column(db.String(500), nullable=False)
+    # likes = db.Column(db.Integer, default=0)
+    # dislikes = db.Column(db.Integer, default=0)
+    # time = db.Column(db.DateTime, default=func.now())
